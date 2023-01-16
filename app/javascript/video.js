c1a458c533b2fba737f967016c2656530f0e9dd1
Samy Rjob <s.rjob@audencia.com>
2023-01-16 16:46:13 +0100

    respojsive 1

---
+++ app/javascript/video.js
@@ -0,0 +1,13 @@
+// const videoPlayer = document.querySelector('.video-player')
+// const video = videoPlayer.querySelector('.togo')
+// const PlayButton = videoPlayer.querySelector('.play-button')
+
+// PlayButton.addEventListener('click', (e) => {
+//   if (video.paused){
+//     video.play()
+//     e.target.textContent = 'I I'
+//   } else {
+//     video.pause()
+//     e.target.textContent = '>'
+//   }
+// })
